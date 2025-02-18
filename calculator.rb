e8266ef9c4e0d2d9ada43ce96a8b5e1274285a7d
rahulmumbai321 <rmkhobragade22@gmail.com>
2025-02-18 22:38:23 +0530

    rspec for sum of two arguments

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,10 @@
-  def calculate_num(*numbers)
-    sum = 0
+
+    def calculate_num(*numbers)
+    sum = 1
     numbers.inject(0){ |sum, x| sum + x } 
+    end
     puts "Enter numbers separated by spaces:"
     user_input = gets.chomp.split(" ").map(&:to_i)
     cal = calculate_num(*user_input)
     puts "The sum is: #{cal}" 
-  end 
+
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,10 +1,8 @@
 require 'calculator.rb'
+require_relative '../calculator.rb'
 
 RSpec.describe "calculate_num" do
-  context "addition of two numbers" do
-    it "returns the sum of multiple numbers" do
-      allow_any_instance_of(Object).to receive(:gets).and_return("3 5")
-      expect(calculate_num).to eq(8)
-    end
+  it "returns the sum of multiple numbers" do
+    expect(calculate_num(1, 2)).to eq(3)
   end
 end
