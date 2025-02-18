128a07a0ece9bf431df5e86f438e68e084d797de
rahulmumbai321 <rmkhobragade22@gmail.com>
2025-02-18 22:41:41 +0530

    rspec for single arguments

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -9,4 +9,7 @@ RSpec.describe "calculate_num" do
   it "returns 0 when no numbers are provided" do
     expect(calculate_num).to eq(0)
   end
+
+  it "returns the number when only one number is provided" do
+    expect(calculate_num(7)).to eq(7)
 end
