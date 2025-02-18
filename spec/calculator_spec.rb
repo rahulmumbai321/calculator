4b0c62f793b97e75cb5cb66401b6bb35218273b7
rahulmumbai321 <rmkhobragade22@gmail.com>
2025-02-18 22:40:25 +0530

    rspec for 0 argumentsr

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe "calculate_num" do
   it "returns the sum of multiple numbers" do
     expect(calculate_num(1, 2)).to eq(3)
   end
+
+  it "returns 0 when no numbers are provided" do
+    expect(calculate_num).to eq(0)
+  end
 end
