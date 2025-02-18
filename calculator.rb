982894fae272f8af3e994b60053caed8bdcccb24
rahulmumbai321 <rmkhobragade22@gmail.com>
2025-02-18 21:17:44 +0530

    calculating and testing

--- calculator.rb
+++ calculator.rb
@@ -3,6 +3,6 @@
     numbers.inject(0){ |sum, x| sum + x } 
     puts "Enter numbers separated by spaces:"
     user_input = gets.chomp.split(" ").map(&:to_i)
-    cal = calculate_sum(*user_input)
+    cal = calculate_num(*user_input)
     puts "The sum is: #{cal}" 
   end 
